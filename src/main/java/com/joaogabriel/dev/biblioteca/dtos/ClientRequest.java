e8a85b3ecf571dbe004efca422f320a233f9ae1f
João Gabriel <joaogabriel.dev12@gmail.com>
2026-07-13 09:44:33 -0300

    create client dtos

---
+++ src/main/java/com/joaogabriel/dev/biblioteca/dtos/ClientRequest.java
@@ -0,0 +1,5 @@
+package com.joaogabriel.dev.biblioteca.dtos;
+
+public record ClientRequest(String nome, String email, String telefone, String cpf, String endereco) {
+    
+}
---
+++ src/main/java/com/joaogabriel/dev/biblioteca/dtos/ClientResponse.java
@@ -0,0 +1,5 @@
+package com.joaogabriel.dev.biblioteca.dtos;
+
+public record ClientResponse(Long id, String nome, String email, String telefone, String cpf, String endereco) {
+    
+}
