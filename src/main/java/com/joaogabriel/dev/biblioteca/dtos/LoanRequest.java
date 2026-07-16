79b7ae0cab50175000587df202a27b1a5ba961ca
João Gabriel <joaogabriel.dev12@gmail.com>
2026-07-16 10:07:51 -0300

    feat: dtos for Loan

---
+++ src/main/java/com/joaogabriel/dev/biblioteca/dtos/LoanRequest.java
@@ -0,0 +1,4 @@
+package com.joaogabriel.dev.biblioteca.dtos;
+
+public record LoanRequest(Long idClient, Long idBook) {
+}
---
+++ src/main/java/com/joaogabriel/dev/biblioteca/dtos/LoanResponse.java
@@ -0,0 +1,18 @@
+package com.joaogabriel.dev.biblioteca.dtos;
+
+import java.time.OffsetDateTime;
+
+import com.joaogabriel.dev.biblioteca.model.Book;
+import com.joaogabriel.dev.biblioteca.model.Client;
+import com.joaogabriel.dev.biblioteca.model.enums.LoanStatus;
+
+public record LoanResponse(
+    Long id, 
+    Client client, 
+    Book book, 
+    LoanStatus status,
+    OffsetDateTime loanDate,
+    OffsetDateTime returnBookDate
+) {
+
+}
