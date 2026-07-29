c5ff42704d6089943b77efd1f214191925ade9c4
João Gabriel <joaogabriel.dev12@gmail.com>
2026-07-29 10:36:35 -0300

    update: add service annotation

--- src/main/java/com/joaogabriel/dev/biblioteca/service/UserDetailsServiceImpl.java
+++ src/main/java/com/joaogabriel/dev/biblioteca/service/UserDetailsServiceImpl.java
@@ -3,10 +3,12 @@ package com.joaogabriel.dev.biblioteca.service;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
 import com.joaogabriel.dev.biblioteca.repository.UserRepository;
 import com.joaogabriel.dev.biblioteca.security.UserAuthenticated;
 
+@Service
 public class UserDetailsServiceImpl implements UserDetailsService {
 
     private final UserRepository userRepository;
