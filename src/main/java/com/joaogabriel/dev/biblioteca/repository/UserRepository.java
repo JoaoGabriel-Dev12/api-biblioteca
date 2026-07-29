6f92a51816756c030c9881d29bb19a6082eae521
João Gabriel <joaogabriel.dev12@gmail.com>
2026-07-29 10:21:24 -0300

    feat: implements UserDetailsService

--- src/main/java/com/joaogabriel/dev/biblioteca/repository/UserRepository.java
+++ src/main/java/com/joaogabriel/dev/biblioteca/repository/UserRepository.java
@@ -4,8 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.joaogabriel.dev.biblioteca.model.User;
+import java.util.Optional;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, Long>{
-    
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
 }
